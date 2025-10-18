b71f02dcd729368a24f305b898db8e93d3c44b5b
Your Name <ktc25a31g0013@edu.kyoto-tech.acjp>
2025-10-18 12:22:12 +0900

    13

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
   base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
 }));
 
